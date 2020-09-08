e3af127f64a2fd9fa24b3718e283fe5f760df45c
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-09-08 16:14:06 +0200

    find submatch

--- document.go
+++ document.go
@@ -73,7 +73,7 @@ func scanurls(path string) ([]string, error) {
  
   for scanner.Scan() {
     line := scanner.Text()
-    url := pattern.FindString(line)
+    url := pattern.FindSubmatch(line)
     fmt.Println(line, "-->", url)
     if( 4 < len(url) ) {
       urls = append(urls, url)
